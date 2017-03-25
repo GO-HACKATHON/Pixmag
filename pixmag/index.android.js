49b515116782f2d90ae48d995a83e9e2354e8f5c
trinotorianto <achmad.trinoto@indotrading.com>
2017-03-25 19:12:04 +0700

    Revert "g"
    This reverts commit 8a6a8feba5c54822d7d03faa009212e913ce4725.

--- pixmag/index.android.js
+++ pixmag/index.android.js
@@ -1,3 +1,5 @@
+
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -6,67 +8,23 @@ import {
   View
 } from 'react-native';
 
-import { GiftedChat } from 'react-native-gifted-chat';
-import { Backend } from './src/Backend.js';
- 
 export default class pixmag extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {messages: []};
-    this.onSend = this.onSend.bind(this);
-  }
-  
-  componentWillMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://facebook.github.io/react/img/logo_og.png',
-          },
-        },
-      ],
-    });
-  }
-  
-  onSend(messages = []) {
-    this.setState((previousState) => {
-      return {
-        messages: GiftedChat.append(previousState.messages, messages),
-      };
-    });
-  }
-
   render() {
     return (
-      <GiftedChat
-        messages={this.state.messages}
-        onSend={this.onSend}
-        user={{
-          _id: 1,
-        }}
-      />
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
     );
   }
-
-  componentDidMount(){
-    Backend.loadChat()  
-  }
-
-  componentWillUnMount(){
-    Backend.closeChat((messages) => {
-      this.setState((previousState) =>{
-          return{
-            messages: GiftedChat.append(previousState.messages, messages)
-          };
-      });
-    });
-  }
-
 }
 
 const styles = StyleSheet.create({
