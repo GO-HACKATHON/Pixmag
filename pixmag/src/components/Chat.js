d1d9214a5dbe1440aeeb4c88b385abff3f944084
trinotorianto <achmad.trinoto@indotrading.com>
2017-03-25 13:44:52 +0700

    komit

---
+++ pixmag/src/components/Chat.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import{
+	View,
+	Text,
+
+} from 'react-native';
+
+class Chat extends React.Component {
+	render(){
+		return(
+			<view>
+				<text>
+					Hallooww  chat!
+				</text>
+			</view>
+		);
+	}
+}
+
+export default Chat;
---
+++ pixmag/src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import{
+	View,
+	Text,
+
+} from 'react-native';
+
+class Home extends React.Component {
+	render(){
+		return(
+			<view>
+				<text>
+					Hallooww im Home!
+				</text>
+			</view>
+		);
+	}
+}
+
+export default Home;
