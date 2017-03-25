e8b1eac5650ed6e934f372e9f5e98f94886f9c53
trinotorianto <achmad.trinoto@indotrading.com>
2017-03-25 20:34:21 +0700

    komeng

--- pixmag/src/components/Chat.js
+++ pixmag/src/components/Chat.js
@@ -6,12 +6,40 @@ import{
 	Button,
 
 } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 class Chat extends React.Component {
-	constructor(){
-		super();
+	constructor(props) {
+    	super(props);
+    	this.state = {messages: []};
+    	this.onSend = this.onSend.bind(this);
+  	}
 
+	componentWillMount() {
+		this.setState({
+		messages: [
+			{
+			_id: 1,
+			text: 'Hello developer',
+			createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+			user: {
+				_id: 2,
+				name: 'React Native',
+				avatar: 'https://facebook.github.io/react/img/logo_og.png',
+			},
+			},
+		],
+		});
 	}
+
+	onSend(messages = []) {
+		this.setState((previousState) => {
+		return {
+			messages: GiftedChat.append(previousState.messages, messages),
+		};
+		});
+	}
+
 	Routes(route, navigator){
 		console.log('tai')
 		if(route.name === "home"){
@@ -22,7 +50,7 @@ class Chat extends React.Component {
 	}
 	render(){
 		return(
-			<View>
+			/*<View>
 				<Text>
 					CHAAAAttttt
 				</Text>
@@ -31,7 +59,14 @@ class Chat extends React.Component {
 	color="#841584"
 	accessibilityLabel="Learn more about this purple button"
 				/>
-			</View>
+			</View>*/
+			<GiftedChat
+				messages={this.state.messages}
+				onSend={this.onSend}
+				user={{
+				_id: 1,
+			}}
+      />
 		);
 	}
 }
