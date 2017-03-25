59e0484b0400c7fb8828754b4f6a73dbf172a60c
Lin <sudarma.lin@gmail.com>
2017-03-25 19:09:37 +0700

    update router home

--- pixmag/index.android.js
+++ pixmag/index.android.js
@@ -1,49 +1,33 @@
 
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  Navigator,
+  TouchableHighlight,
 } from 'react-native';
 
+import Chat from './src/components/Chat.js';
+import Home from './src/components/Home.js';
+import Styles from './src/components/style.js';
+
+
+
 export default class pixmag extends Component {
+  Routes(route, navigator){
+    if(route.name === "home"){
+      return <Home navigator={navigator}/>
+    }else if (route.name === 'chat'){
+      return <Chat navigator ={navigator}/>
+    }
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Navigator
+      initialRoute={{name :'home'}}
+      renderScene={this.Routes.bind(this) }
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('pixmag', () => pixmag);
--- pixmag/src/components/Chat.js
+++ pixmag/src/components/Chat.js
@@ -3,17 +3,35 @@ import React from 'react';
 import{
 	View,
 	Text,
+	Button,
 
 } from 'react-native';
 
 class Chat extends React.Component {
+	constructor(){
+		super();
+
+	}
+	Routes(route, navigator){
+		console.log('tai')
+		if(route.name === "home"){
+			return <Home navigator={navigator}/>
+		}else if (route.name === 'chat'){
+			return <Chat navigator ={navigator}/>
+		}
+	}
 	render(){
 		return(
-			<view>
-				<text>
-					Hallooww  chat!
-				</text>
-			</view>
+			<View>
+				<Text>
+					CHAAAAttttt
+				</Text>
+				<Button onPress={()=> this.props.navigator.pop()}
+				title="Learn More"
+	color="#841584"
+	accessibilityLabel="Learn more about this purple button"
+				/>
+			</View>
 		);
 	}
 }
